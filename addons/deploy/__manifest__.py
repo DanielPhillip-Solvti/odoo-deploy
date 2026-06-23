61b38f7265ff7446175a8697226486f0ccb1356f
Philip Ziobrowski <philip.ziobrowski@solvti.com>
2026-06-23 22:32:05 +0000

    Commit of all changes

---
+++ addons/deploy/__manifest__.py
@@ -0,0 +1,27 @@
+{
+    "name": "Deploy",
+    "summary": "Module for deploying Odoo on a server.",
+    "version": "19.0.0.0.1",
+    "author": "Solvti Sp. z o.o.",
+    "license": "LGPL-3",
+    "category": "Tools",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/deploy_environment_views.xml",
+        "views/deploy_vm_views.xml",
+        "views/deploy_menu_items.xml",
+        "views/github_app_config_views.xml",
+        "data/github_app_config.xml",
+    ],
+    "assets": {
+        "web.assets_backend": [
+            "deploy/static/src/components/github_commit_history/github_commit_history.js",
+            "deploy/static/src/components/github_commit_history/github_commit_history.xml",
+            "deploy/static/src/scss/deploy_style.scss",
+        ],
+    },
+    "depends": ["obscure_field"],
+    "installable": True,
+    "application": True,
+    "reusable": True,
+}
---
+++ addons/obscure_field/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Obscure Field",
+    "version": "19.0.1.0.0",
+    "category": "Technical",
+    "summary": "Store sensitive character fields without exposing values in RPC/read/export responses",
+    "author": "Solvti Sp. z o.o.",
+    "license": "LGPL-3",
+    "price": 0,
+    "currency": "USD",
+    "depends": ["base", "web"],
+    "data": [],
+    "images": [
+        "static/description/icon.png",
+    ],
+    "website": "https://github.com/tuanhoangdef/obscure_field/tree/19.0/",
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "reusable": True,
+}
